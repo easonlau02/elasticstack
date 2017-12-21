bfff76ad907cb0b7ef76f093a34003b305c0d010
eason lau <eason.lau@cargosmart.com>
2017-12-20 20:29:33 -0500

    Upgrade elk to 5.3.1

---
+++ Dockerfile.java
@@ -0,0 +1,11 @@
+# From base OS image
+FROM eason02/centos:7
+
+# Maintainer
+MAINTAINER eason.lau02@hotmail.com
+
+RUN yum -y update && \
+    yum -y install wget && \
+    wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm" && \
+    yum -y localinstall jdk-8u112-linux-x64.rpm && \
+    rm -f ~/jdk-8u112-linux-x64.rpm
